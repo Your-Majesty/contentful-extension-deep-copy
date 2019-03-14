57a9371765c5c71ae922b6a523a9c3ee3c87c57b
Hugo Wiledal <hugo.wiledal@gmail.com>
2019-03-14 18:01:01 +0100

    Build version

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -1316,7 +1316,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57259" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49659" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
