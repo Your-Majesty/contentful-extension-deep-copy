11ef1907914f812be22d12550627a4ca4fc962e3
Hugo Wiledal <hugo.wiledal@gmail.com>
2019-03-15 11:07:00 +0100

    Updated to include contentful-like styles

--- source/app.js
+++ source/app.js
@@ -1,108 +1,57 @@
 import { init as initContentfulExtension } from 'contentful-ui-extensions-sdk'
+import { recursiveClone } from './deep-copy'
 
 let space = null
 let entry = null
+let extension = null
+let state = 'idle'
 
-initContentfulExtension(extension => {
-  console.log('im am inited')
-  space = extension.space
-  entry = extension.entry
+const activationButton = document.querySelector('button')
+const logWindow = document.querySelector('.log-window')
+
+initContentfulExtension(getExtension => {
+  space = getExtension.space
+  entry = getExtension.entry
+  extension = getExtension
+
+  if (extension.window.updateHeight) extension.window.updateHeight()
 })
 
-function doLog (str) {
-  const ev = new CustomEvent('deepclonelog', {
-    detail: {
-      log: str
-    }
-  })
 
-  window.dispatchEvent(ev)
+function addToLog (str) {
+  logWindow.innerHTML += `<p>${str}</p>`
+  logWindow.scrollTo(0, 999999999)
 }
-
-async function getUpdatedField(field, tag, depth) {
-  if (field && Array.isArray(field)) {
-    doLog(getLine(depth) + '| Field is Array, looping through')
-    return await Promise.all(field.map(async (f) => {
-      return await getUpdatedField(f, tag, depth + 1)
-    }))
-  }
-  
-  if (field && field.sys && field.sys.type === 'Link' && field.sys.linkType === 'Entry') {
-    doLog(getLine(depth) + '| Field is Entry Link, cloning')
-    const clonedEntry = await recursiveClone(field.sys.id, tag, depth + 1)
-    field.sys.id = clonedEntry.sys.id
-  }
-
-  return field
-}
-
-async function getUpdatedFields(fields, tag, depth) {
-  doLog(getLine(depth) + '| Get fields')
-  for (let fieldName in fields) {
-    const field = fields[fieldName]
-
-    for (let lang in field) {
-      fields[fieldName][lang] = await getUpdatedField(fields[fieldName][lang], tag, depth)
-    }
-  }
-
-  return fields
-}
-
-async function createNewEntry (type, data) {
-  return await space.createEntry(type, data)
-}
-
-async function getEntry (id) {
-  return await space.getEntry(id)
-}
-
-function getLine (depth) {
-  return Array(depth).join('--')
-} 
-
-async function recursiveClone (id, tag, depth) {
-  doLog(getLine(depth) + '| Recursive clone ' + id)
-  const entry = await getEntry(id)
-
-  const newFields = await getUpdatedFields(entry.fields, tag, depth + 1)
-  if (newFields.title && newFields.title['en-US']) {
-    newFields.title['en-US'] = newFields.title['en-US'] + ' (copy ' + tag + ')'
-  }
-  const newEntry = await createNewEntry(entry.sys.contentType.sys.id, {fields: newFields})
-
-  return newEntry
-}
-async function performDeepCopy (id) {
-  doLog('DEEEP COPY: ' + id)
-
-  const tag = new Date().toUTCString()
-  const depth = 0
-
-  return await recursiveClone(id, tag, depth)
-}
-
-window.addEventListener('deepclonelog', (event) => {
-  document.body.innerHTML += `<div>${event.detail.log}</div>`
-  window.scrollTo(0, 100000)
+window.addEventListener('deepcopylog', (event) => {
+  addToLog(event.detail.log)
 })
 
 window.doTheDeepCopy = async function() {
+  if (state != 'idle') return
+
+  state = 'cloning'
+
+  activationButton.classList.add('cf-is-loading')
+  activationButton.disabled = true
+  logWindow.style.display = 'block'
+
+  const tag = document.querySelector('.clone-tag').value
+
+  if (extension.window.updateHeight) extension.window.updateHeight()
+
   const sys = entry.getSys()
-  alert('Deep Copying this entry.')
-  document.body.style.background = 'green'
-  document.body.innerHTML = 'LOADING...'
-  const clonedEntry = await performDeepCopy(sys.id)
-  alert('Done!')
+  addToLog('Clone started...')
+  const clonedEntry = await recursiveClone(space, sys.id, tag, 0)
+  addToLog('')
+  addToLog('<strong>Clone successful!<strong>')
+  addToLog('New entry at:')
+  addToLog(`<a target="_top" href="https://app.contentful.com/spaces/${sys.space.sys.id}/entries/${clonedEntry.sys.id}">https://app.contentful.com/spaces/${sys.space.sys.id}/entries/${sys.id}</a>`)
 
-  doLog('-')
-  doLog('+++++++++++++++++++++++++++++++++++')
-  doLog(' + We are finished +')
-  doLog('+++++++++++++++++++++++++++++++++++')
+  activationButton.classList.remove('cf-is-loading')
+}
 
-  document.body.style.background = 'white'
+document.querySelector('.clone-tag').value = `(${new Date().toUTCString()})`
 
-  document.body.innerHTML += `<a target="_top" href="https://app.contentful.com/spaces/${sys.space.sys.id}/entries/${clonedEntry.sys.id}">https://app.contentful.com/spaces/${sys.space.sys.id}/entries/${sys.id}</a>`
-
-  // window.top.location.href = `https://app.contentful.com/spaces/${sys.space.sys.id}/entries/${sys.id}`
+window.toggleConfirmModal = function (flag) {
+  document.querySelector('.confirm').style.display = flag ? 'flex' : 'none'
 }
---
+++ source/deep-copy.js
@@ -0,0 +1,71 @@
+function log (str) {
+  if (window) {
+    const ce = new CustomEvent('deepcopylog', {
+      detail: {
+        log: str
+      }
+    })
+    window.dispatchEvent(ce)
+  }
+  if (console) {
+    console.log('DeepCopy: ' + str)
+  }
+}
+
+async function getUpdatedField(space, field, tag, depth) {
+  if (field && Array.isArray(field)) {
+    return await Promise.all(field.map(async (f) => {
+      return await getUpdatedField(space, f, tag, depth + 1)
+    }))
+  }
+  
+  if (field && field.sys && field.sys.type === 'Link' && field.sys.linkType === 'Entry') {
+    log(`------ Field contains reference, performing clone...`)
+    const clonedEntry = await recursiveClone(space, field.sys.id, tag, depth + 1)
+    field.sys.id = clonedEntry.sys.id
+  }
+
+  return field
+}
+
+async function getUpdatedFields(space, fields, tag, depth) {
+  for (let fieldName in fields) {
+    const field = fields[fieldName]
+
+    for (let lang in field) {
+      log(`---- Getting fields for ${lang}...`)
+      fields[fieldName][lang] = await getUpdatedField(space, fields[fieldName][lang], tag, depth)
+    }
+  }
+
+  return fields
+}
+
+async function createNewEntry (space, type, data) {
+  log(`------ + Creating new entry of type "${type}"...`)
+  return await space.createEntry(type, data)
+}
+
+async function getEntry (space, id) {
+  return await space.getEntry(id)
+}
+
+async function recursiveClone (space, id, tag, depth) {
+  log(`Cloning ${id}...`)
+  const entry = await getEntry(space, id)
+
+  log(`-- Getting fields for ${id}...`)
+  const newFields = await getUpdatedFields(space, entry.fields, tag, depth + 1)
+  if (newFields.title && newFields.title['en-US']) {
+    newFields.title['en-US'] = newFields.title['en-US'] + ' ' + tag
+  }
+  const newEntry = await createNewEntry(space, entry.sys.contentType.sys.id, {fields: newFields})
+
+  log(`Clone of ${id} completed.`)
+
+  return newEntry
+}
+
+export {
+  recursiveClone
+}
