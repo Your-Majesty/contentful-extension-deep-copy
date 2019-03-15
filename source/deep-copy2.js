057016863981489e8346e9db2b60be8d9582a685
Hugo Wiledal <hugo.wiledal@gmail.com>
2019-03-15 13:48:12 +0100

    Added rate-limiting prevetions, and status updates

--- source/deep-copy2.js
+++ source/deep-copy2.js
@@ -1,6 +1,33 @@
 import { log } from './log'
 
 let references = {}
+let referenceCount = 0
+let newReferenceCount = 0
+let updatedReferenceCount = 0
+
+const statusUpdateTimeout = 3000
+const waitTime = 10
+
+async function wait (ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+async function updateEntry (space, entry) {
+  await wait(waitTime)
+  return await space.updateEntry(entry)
+}
+
+async function createEntry (space, type, data) {
+  await wait(waitTime)
+  return await space.createEntry(type, data)
+}
+
+async function getEntry (space, entryId) {
+  await wait(waitTime)
+  return await space.getEntry(entryId)
+}
 
 async function inspectField (space, field) {
   if (field && Array.isArray(field)) {
@@ -15,11 +42,14 @@ async function inspectField (space, field) {
 }
 
 async function findReferences (space, entryId) {
-  const entry = await space.getEntry(entryId)
-
   if (references[entryId]) {
     return
   }
+
+  const entry = await getEntry(space, entryId)
+
+  referenceCount++
+
   references[entryId] = entry
 
   for (let fieldName in entry.fields) {
@@ -39,7 +69,8 @@ async function createNewEntriesFromReferences (space, tag) {
   for (let entryId in references) {
     const entry = references[entryId]
     if (entry.fields.title && entry.fields.title['en-US']) entry.fields.title['en-US'] = entry.fields.title['en-US'] + ' ' + tag
-    const newEntry = await space.createEntry(entry.sys.contentType.sys.id, { fields: entry.fields })
+    const newEntry = await createEntry(space, entry.sys.contentType.sys.id, { fields: entry.fields })
+    newReferenceCount++
     newEntries[entryId] = newEntry
   }
   
@@ -74,29 +105,49 @@ async function updateReferenceTree(space, newReferences) {
       }
     }
 
-    await space.updateEntry(entry)
+    await updateEntry(space, entry)
+    updatedReferenceCount++
   }
 }
 
 async function recursiveClone (space, entryId, tag) {
   references = {}
+  referenceCount = 0
+  newReferenceCount = 0
+  updatedReferenceCount = 0
   log(`Starting clone...`)
+
+  let statusUpdateTimer = null
 
   log('')
   log(`Finding references recursively...`)
+
+  statusUpdateTimer = setInterval(() => {
+    log(` - found ${referenceCount} entries so far...`)
+  }, statusUpdateTimeout)
+
   await findReferences(space, entryId)
-  const numReferences = Object.keys(references).length
-  log(` -- Found ${numReferences} reference(s)`)
+  clearInterval(statusUpdateTimer)
+  log(` -- Found ${referenceCount} reference(s) in total`)
 
   log('')
   log(`Creating new entries...`)
+
+  statusUpdateTimer = setInterval(() => {
+    log(` - created ${newReferenceCount}/${referenceCount} - ${Math.round((newReferenceCount / referenceCount) * 100)}%`)
+  }, statusUpdateTimeout)
+
   const newReferences = await createNewEntriesFromReferences(space, tag)
-  const numNewReferences = Object.keys(newReferences).length
-  log(` -- Created ${numNewReferences} reference(s)`)
+  clearInterval(statusUpdateTimer)
+  log(` -- Created ${newReferenceCount} reference(s)`)
 
   log('')
   log(`Updating reference-tree...`)
+  statusUpdateTimer = setInterval(() => {
+    log(` - updated ${updatedReferenceCount}/${referenceCount} - ${Math.round((updatedReferenceCount / referenceCount) * 100)}%`)
+  }, statusUpdateTimeout)
   await updateReferenceTree(space, newReferences)
+  clearInterval(statusUpdateTimer)
 
   log('')
   log(`Updating done.`)
